ad7f23115596a324b69d4263b3612388fb0973eb
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-10 19:33:21 +0330

    inheritance1

---
+++ clean_class.py
@@ -0,0 +1,14 @@
+class Animal:
+    fur_color = 'Orange'
+
+    def speak(self):
+        print("Raaww")
+
+    def eat(self):
+        pass
+
+    def chase(self):
+        pass
+
+tiger = Animal()
+tiger.speak()
