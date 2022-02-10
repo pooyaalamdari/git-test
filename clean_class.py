b42f0d92ad5acb56006ca5b64b2dd584c7f476c5
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-10 23:31:08 +0330

    super() method

--- clean_class.py
+++ clean_class.py
@@ -5,7 +5,7 @@ class Animal:
         raise NotImplementedError
 
     def eat(self):
-        pass
+        print('I am eating yum yum yum!')
 
     def chase(self):
         pass
@@ -14,10 +14,16 @@ class Tiger(Animal):
 
     fur_color = 'Black'
 
-
-
+    def eat(self):
+        #if we want to run eat method in Animal class
+        #then eat method in Tiger class
+        #super().eat()
+        #we can use self in super().eat(self)
+        #execute Animal class and eat method in Animal class
+        super().eat()
+        print('I am eating salmon')
 
 #type(tiger) -> Tiger Class
 #NOT Animal class
 tiger = Tiger()
-tiger.speak()
+tiger.eat()
