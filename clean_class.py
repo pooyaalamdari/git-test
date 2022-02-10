f32093b52763035452a37722e471394b0f7aeac7
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-10 23:50:19 +0330

    dunder

--- clean_class.py
+++ clean_class.py
@@ -1,6 +1,10 @@
 class Animal:
     fur_color = 'Orange'
 
+    #1-at the first this code line was running :D
+    def __init__(self, fur_color):
+        print(f"Fur color is {fur_color}")
+
     def speak(self):
         raise NotImplementedError
 
@@ -8,6 +12,7 @@ class Animal:
         print('I am eating yum yum yum!')
 
     #by default -> Gazeller
+    #2-then this code was run
     def chase(self, animal='Gazelle'):
         print('I am chasing', animal)
 
@@ -26,11 +31,12 @@ class Tiger(Animal):
 
     #chase is (Animal class) method
     # animal is (Animal class) chase method's argument
+    #3-at the last this code was run
     def chase(self,animal):
         super().chase(animal)
         print(animal,'was caught')
 
 #type(tiger) -> Tiger Class
 #NOT Animal class
-tiger = Tiger()
-tiger.chase('rabit') 
+tiger = Tiger('Grey')
+tiger.chase('rabit')
