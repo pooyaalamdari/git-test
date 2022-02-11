24d957330034e02d7b9f84f0383bb9849917810f
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-11 14:58:02 +0330

    error2 episode2

--- error2.py
+++ error2.py
@@ -1,6 +1,10 @@
 num = input('What is a number? ')
 try:
     num = int(num)
+#if we got error ValueError block will run
+except ValueError:
+    print(num, 'was not a valid number')
+# these are will not run
 except Exception as e:
     print('Exception was run')
     print(type(e))
